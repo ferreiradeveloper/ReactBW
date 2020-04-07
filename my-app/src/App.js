be9e0d69eabc03925cb91ec660edb67b1ccc0a29
Francisco Ferreira <45833609+ferreiradeveloper@users.noreply.github.com>
2020-04-07 23:42:10 +0200

    hello

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Parrafo from './componets/Parrafo';
+import Variables from './componets/Variables';
+
+function App() {
+  return (
+     <div>
+  
+    <h1>Hola Mundo React</h1>
+    <Parrafo />
+    <Variables/>
+
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/componets/Parrafo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Parrafo = () => {
+    return (
+        <div>
+            <p>Wellcome to my WebPage</p>
+        </div>
+    )
+}
+
+export default Parrafo
